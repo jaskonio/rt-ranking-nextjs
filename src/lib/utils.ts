db2021cf5f86d1702ec2b15dfbad80883bf7a51c
DESKTOP-2UVI5A0\jonatan.lopez <joalopez@eservicios.indra.es>
2024-11-20 10:07:46 +0100

    added shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,33 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 const paceToSeconds = (pace: string): number => {
-    const match = pace.match(/(\d+)m(\d+)s\/km/);
-    if (!match) {
-        throw new Error(`Invalid pace format: ${pace}`);
-    }
-    const [_, minutes, seconds] = match;
-    return parseInt(minutes) * 60 + parseInt(seconds);
+  const match = pace.match(/(\d+)m(\d+)s\/km/);
+  if (!match) {
+      throw new Error(`Invalid pace format: ${pace}`);
+  }
+  const [_, minutes, seconds] = match;
+  return parseInt(minutes) * 60 + parseInt(seconds);
 };
 
 export const sortPaces = (paceA: string, paceB: string, order: 'ASC' | 'DESC'): number => {
-    const secondsA = paceToSeconds(paceA)
-    const secondsB = paceToSeconds(paceB)
-    return order === 'ASC' ? secondsA - secondsB : secondsB - secondsA;
+  const secondsA = paceToSeconds(paceA)
+  const secondsB = paceToSeconds(paceB)
+  return order === 'ASC' ? secondsA - secondsB : secondsB - secondsA;
 };
 
 
 const timeToSeconds = (time: string): number => {
-    const [hours, minutes, seconds] = time.split(':').map(Number);
-    return hours * 3600 + minutes * 60 + seconds;
+  const [hours, minutes, seconds] = time.split(':').map(Number);
+  return hours * 3600 + minutes * 60 + seconds;
 };
 
 export const sortTimes = (timeA: string, timeB: string, order: 'ASC' | 'DESC'): number => {
-    const secondsA = timeToSeconds(timeA);
-    const secondsB = timeToSeconds(timeB);
-    return order === 'ASC' ? secondsA - secondsB : secondsB - secondsA;
+  const secondsA = timeToSeconds(timeA);
+  const secondsB = timeToSeconds(timeB);
+  return order === 'ASC' ? secondsA - secondsB : secondsB - secondsA;
 };
